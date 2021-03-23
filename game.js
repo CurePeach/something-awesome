110fabb4a53ea1a97e98357ffd1bd5b5aecff734
Clarissa T <ctatang@gmail.com>
2021-03-23 19:35:15 +1100

    Added colour palette

--- game.js
+++ game.js
@@ -2,10 +2,13 @@
  * Configuration file
  */
 
+// Colour palette: https://www.canva.com/colors/color-palettes/strong-and-curved/
+
 const config = {
+  type: Phaser.AUTO,
   width: 800,
   height: 600,
-  backgroundColor: 0x99ffc2,
+  backgroundColor: 0x006884,
   scene: [
     HomeScreen,
     STOScreen
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,31 +7,31 @@
 class HomeScreen extends Phaser.Scene {
   constructor() {
     super("TitleScreen");
+
+    this.boxColour = 0x053D57;
   }
 
   create() {
-    console.log("Creating scene");
-
-    // Links to the different title screens
-    const spofLink = this.add.rectangle(400, 400, 400, 30, 0x000);
+    // Links to the different levels
+    const spofLink = this.add.rectangle(400, 400, 400, 30, this.boxColour);
     this.add.text(210, 390, "Single point of failure");
     spofLink.setInteractive();
     spofLink.on("pointerdown", () => {
       console.log("Clicked!");
     });
 
-    const seLink = this.add.rectangle(400, 450, 400, 30, 0x000);
+    const seLink = this.add.rectangle(400, 450, 400, 30, this.boxColour);
     this.add.text(210, 440, "Social engineering");
     seLink.setInteractive();
     seLink.on("pointerdown", () => {
       console.log("Clicked!");
     });
 
-    const stoLink = this.add.rectangle(400, 500, 400, 30, 0x000);
+    const stoLink = this.add.rectangle(400, 500, 400, 30, this.boxColour);
     this.add.text(210, 490, "Security through obscurity");
     stoLink.setInteractive();
     stoLink.on("pointerdown", () => {
-      this.scene.start("SecurityThroughObscurity");
+      this.scene.start("STOScreen");
     });
   }
 }
